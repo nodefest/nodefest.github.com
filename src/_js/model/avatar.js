a2f27ab7fd019942047b9da358631bcea3c546fb
hokaccha <k.hokamura@gmail.com>
2012-09-17 15:59:16 +0900

    positionに上限と下限を設定

--- src/_js/model/avatar.js
+++ src/_js/model/avatar.js
@@ -2,6 +2,8 @@
 
 var socket = Nodefest.socket;
 var MOVE_ARROW_KEY_DIST = 20;
+var TOP_LIMIT = 10000;
+var LEFT_LIMIT = 5000;
 
 /**
  * アバター一体を管理するModel
@@ -24,6 +26,43 @@ Nodefest.Model.Avatar = Backbone.Model.extend({
         socket.emit('update:text', self.get('text'));
       }
     });
+  },
+
+  /**
+   * Backbone.Model.prototype.setを上書き
+   *
+   * @method set
+   * @param key {String}
+   * @param value {Mixed}
+   * @param options {Object}
+   * @return {String}
+   */
+  set: function(key, value, options) {
+      var attrs, attr, val;
+
+      if (_.isObject(key) || key === null || key === undefined) {
+        attrs = key;
+        options = value;
+      } else {
+        attrs = {};
+        attrs[key] = value;
+      }
+
+      var top, left;
+      if (attrs.position) {
+        top = parseInt(attrs.position.top, 10) || 0;
+        left = parseInt(attrs.position.left, 10) || 0;
+
+        if (top < 0) top = 0;
+        if (top > TOP_LIMIT) top = TOP_LIMIT;
+
+        if (left < 0) left = 0;
+        if (left > LEFT_LIMIT) left = LEFT_LIMIT;
+
+        attrs.position = { top: top, left: left };
+      }
+
+      return Backbone.Model.prototype.set.call(this, attrs, options);
   },
 
   /**
