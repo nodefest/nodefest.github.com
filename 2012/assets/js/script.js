3594846d16b353367e9417130bdc735634469be4
hokaccha <k.hokamura@gmail.com>
2012-09-19 00:25:57 +0900

    socket.ioの向き先を一時変更

--- 2012/assets/js/script.js
+++ 2012/assets/js/script.js
@@ -7,7 +7,7 @@ var Nodefest = window.Nodefest = {
 };
 
 if (window.io) {
-  Nodefest.socket = io.connect('http://210.152.156.43:80/');
+  Nodefest.socket = io.connect('http://49.212.31.180:9000/');
 }
 else {
   Nodefest.socket = {
