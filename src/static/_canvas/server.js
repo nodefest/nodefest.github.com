85953569cf31b1807759055ef3f1e85ebc44a218
yomotsu <oyamada@pxgrid.com>
2013-08-30 13:08:50 +0900

    add socket demo

---
+++ src/static/_canvas/server.js
@@ -0,0 +1,31 @@
+var io = require( 'socket.io' ).listen( 8000 );
+var players = {};
+
+io.sockets.on( 'connection', function ( socket ) {
+
+  socket.on( 'client_firstConnect', function ( data ) {
+    players[ socket.id ] = {
+      points : data.points
+    };
+    io.sockets.socket( socket.id ).emit( 'server_myId', {
+      myID : socket.id,
+      players : players
+    } );
+  });
+
+  socket.on( 'client_pushMyData', function ( data ) {
+    console.log( data.id );
+    players[ data.id ] = {
+      points: data.points
+    };
+  } );
+
+  socket.on( 'disconnect', function () {
+    delete players[ socket.id ];
+  } );
+});
+
+( function loop () {
+    setTimeout( loop, 500 );
+    io.sockets.emit( 'server_sync', players );
+} )();
