c77754edf6d8bb1bbce59f0a48567e359e55b339
Kazuhito Hokamura <k.hokamura@gmail.com>
2013-09-27 00:38:08 +0900

    エフェクトのスクリプト改修

--- src/static/assets/js/src/dotEffect.js
+++ src/static/assets/js/src/dotEffect.js
@@ -1,221 +1,253 @@
-( function () {
-  var DOT_RADIUS = 3;
+(function() {
 
-  var RAF = ( function(){
-    return window.requestAnimationFrame       ||
-           window.webkitRequestAnimationFrame ||
-           function( callback ){
-               window.setTimeout( callback, 1000.0 / 60.0 );
-           };
-  } )();
+'use strict';
 
-  var $el = $( '#dotEffect' );
-  var $canvas = $( '<canvas/>' );
-  $el.append( $canvas );
-  $canvas.attr( {
-    'width' : window.innerWidth,
-    'height': window.innerHeight
-  } );
+var supportDevice = !navigator.userAgent.match(/iPhone|iPad|Android/);
+if (!supportDevice) return;
 
-  ctx = $canvas[ 0 ].getContext( '2d' );
+var PROT_INTERVAL = 500;
+var DOT_RADIUS = 3;
 
-  var me = {
-    id : null,
-    points : [
-      //{
-      //  x: 200,
-      //  y: 100,
-      //  timestamp: Date.now()
-      //}
-    ]
+var $canvas = $('<canvas>');
+var canvas = $canvas.get(0);
+
+$canvas.appendTo('#dotEffect').attr({
+  'width' : window.innerWidth,
+  'height': window.innerHeight
+});
+
+$(window).on('resize', function(e) {
+  $canvas.attr({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+});
+
+var ctx = canvas.getContext('2d');
+var me = new User();
+
+var socket;
+if (window.io) {
+  socket = io.connect( 'http://' + window.__socketServer );
+
+  socket.emit('create', function(result) {
+    $.each(result.users, function(id, point) {
+      if (result.id === id) {
+        me.setId(id);
+      }
+      else {
+        new User({ id: id, point: point });
+      }
+    });
+  });
+
+  socket.on('create', function(id) {
+    new User({ id: id });
+  });
+
+  socket.on('update', function(data) {
+    User.get(data.id).setPoint(data.point);
+  });
+
+  socket.on('destroy', function(id) {
+    User.destroy(id);
+  });
+}
+
+$(window).mousemove(throttle(function(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  var moveX = Math.abs(me.lastPoint.x - x);
+  var moveY = Math.abs(me.lastPoint.y - y);
+  var moved = me.points.length === 0 || moveX > 5 || moveY > 5;
+
+  if (moved) {
+    me.setPoint({ x: x, y: y });
+
+    if (socket) {
+      socket.emit('update', { id: me.id, point: me.lastPoint });
+    }
+  }
+}, PROT_INTERVAL));
+
+var requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  function(fn) {
+    window.setTimeout(fn, 1000.0 / 60.0 );
   };
-  var players = {};
 
-  var myMouseX, myMouseY;
-  $( window ).on( 'mousemove', function ( e ) {
-    myMouseX = e.clientX;
-    myMouseY = e.clientY;
-  } );
+(function draw() {
+  var now = Date.now();
+  requestAnimationFrame(draw);
 
-  $( window ).on( 'resize', function ( e ) {
-    $canvas.attr( {
-      'width' : window.innerWidth,
-      'height': window.innerHeight
-    } );
-  } );
+  canvas.width = canvas.width; // same as `ctx.crearRect`
 
-  function plot () {
-    var data = {
-      x: myMouseX,
-      y: myMouseY,
-      timestamp: Date.now()
-    }
-    me.points.push( data );
-    if ( me.points.length > 5 ) {
-      me.points.shift();
-    }
-  }
+  User.list.forEach(function(user) {
+    var points = user.points;
+    var isMe = user.isMe;
+    var lastPoint = user.lastPoint;
+    var point, x1, x2, y1, y2, lineProgress, dotProgress, isStop;
 
-  var protInterval = setInterval( plot, 500 );
+    for (var i = 0, len = points.length; i < len; i++) {
+      point = points[i];
+      x1 = null;
+      y1 = null;
+      x2 = point.x;
+      y2 = point.y;
+      isStop = false;
 
-  ( function drawPoints () {
-    var i, j, l;
-    var now = Date.now();
-    RAF( drawPoints );
-    // setTimeout( drawPoints, 5000 )
-
-    $canvas[ 0 ].width = $canvas[ 0 ].width;
-
-    for ( i in players ) {
-      ( function () {
-        if ( me.id === i ) {
-          return;
-        }
-        var points = players[ i ].points;
-        for ( j = 0, l = points.length; j < l; j ++ ) {
-          if ( j != 0 ) {
-            var x1 = points[ j - 1 ].x;
-            var y1 = points[ j - 1 ].y;
-          }
-          var x2 = points[ j ].x;
-          var y2 = points[ j ].y;
-          var timestamp = points[ j ].timestamp * 1;
-
-          var progress = Math.min( ( now - timestamp ) / 2000, 1 );
-
-        // console.log( timestamp, ( now - timestamp ) / 2000 );
-        // console.log( x1, y1, x2, y2, progress );
-          drawLine( x1, y1, x2, y2, progress );
-          drawDot( x2, y2, progress );
-        }
-      } )();
-    }
-
-    for ( i = 0, l = me.points.length; i < l; i ++ ) {
-      if ( i != 0 ) {
-        var x1 = me.points[ i - 1 ].x;
-        var y1 = me.points[ i - 1 ].y;
+      if (i !== 0) {
+        x1 = points[i - 1].x;
+        y1 = points[i - 1].y;
       }
-      var x2 = me.points[ i ].x;
-      var y2 = me.points[ i ].y;
-      var timestamp = me.points[ i ].timestamp;
-      var progress = Math.min( ( now - timestamp ) / 2000, 1 );
-      drawLine( x1, y1, x2, y2, progress, { me: true } );
-      drawDot( x2, y2, progress, { me: true } );
-    }
-  } )();
 
-  function drawDot ( x, y, progress, params ) {
-    if ( ! ( 0 < progress && progress < 1 ) ) {
-      return;
-    };
-    var COLOR1 = params && params.me ? [ 82, 204, 186 ] : [ 255, 153, 167 ];
-    var COLOR2 = params && params.me ? [ 255, 248, 59 ] : [ 115, 153, 230 ];
-    var color = [];
+      lineProgress = dotProgress = (now - point.timestamp) / 2000;
 
-    var localProgress1 = getLocalProgress( 0, 0.1, progress );
-    var r = DOT_RADIUS - 3 + ( 3 * localProgress1 );
-    var r2 = DOT_RADIUS + 10 - ( 6 * localProgress1 );
-
-    for ( var i = 0; i < 3; i ++ ) {
-      var localProgress2 = getLocalProgress( 0.5, 0.8, progress );
-      color[ i ] = ( COLOR1[ i ] + ( COLOR2[ i ] - COLOR1[ i ] ) * localProgress2 )|0;
-    }
-    var alpha = 1 - getLocalProgress( 0.9, 1, progress );
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.arc( x, y, r, 0, Math.PI * 2, false );
-    ctx.fillStyle = 'rgba( ' + color[ 0 ] + ', ' + color[ 1 ] + ', ' + color[ 2 ] + ',' + alpha + ' )';
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc( x, y, r2, 0, Math.PI * 2, false );
-    ctx.fillStyle = 'rgba( ' + color[ 0 ] + ', ' + color[ 1 ] + ', ' + color[ 2 ] + ', ' + Math.min( 0.2, alpha ) + ')';
-    ctx.fill();
-
-    ctx.restore();
-  }
-
-  function drawLine ( x1, y1, x2, y2, progress, params ) {
-    if ( ! ( 0 < progress && progress < 1 ) ) {
-      return;
-    };
-    if ( !x1 || !y1 || !x2 || !y2 ) {
-      return;
-    }
-
-    var lengthOfLine = getLocalProgress( 0, 0.1, progress, params );
-    var m = lengthOfLine;
-    var n = 1 - lengthOfLine;
-    var x3 = n * x1 + m * x2;
-    var y3 = n * y1 + m * y2;
-    var grad = getGrad( x1, y1, x3, y3, lengthOfLine, progress, params );
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.moveTo( x1, y1 );
-    ctx.lineTo( x3, y3 );
-    ctx.closePath();
-    ctx.strokeStyle = grad;
-    ctx.stroke();
-    ctx.restore();
-  }
-
-  function getGrad( x1, y1, x2, y2, lengthOfLine, progress, params ) {
-    var COLOR1 = params && params.me ? [ 82, 204, 186 ] : [ 255, 153, 167 ];
-    var COLOR2 = params && params.me ? [ 255, 248, 59 ] : [ 115, 153, 230 ];
-    var color = [];
-
-    var grad  = ctx.createLinearGradient( x1, y1, x2, y2 );
-    if ( 0 < progress && progress < 0.1 ) {
-      grad.addColorStop( 0, 'rgb(' + COLOR2.join() + ')' );
-      grad.addColorStop( 1, 'rgb(' + COLOR1.join() + ')' );
-      return grad;
-    }
-    if ( progress < 0.6 ) {
-      var localProgress = getLocalProgress( 0.1, 0.6, progress );
-      for ( var i = 0; i < 3; i ++ ) {
-        color[ i ] = ( COLOR1[ i ] + ( COLOR2[ i ] - COLOR1[ i ] ) * localProgress )|0;
+      if (point === lastPoint && dotProgress > 0.4) {
+        if (!lastPoint._timestamp || now - lastPoint._timestamp > 600) {
+          lastPoint._timestamp = now - 20;
+        }
+        dotProgress = (now - lastPoint._timestamp) / 2000;
+        isStop = true;
       }
-      grad.addColorStop( 0, 'rgb(' + COLOR2.join() + ')' );
-      grad.addColorStop( 1, 'rgb(' + color.join() + ')' );
-      return grad;
+      else if (point._timestamp) {
+        dotProgress = (now - point._timestamp) / 2000;
+      }
+
+      if (0 < lineProgress && lineProgress < 1) {
+        drawLine(x1, y1, x2, y2, lineProgress, { me: isMe });
+      }
+
+      if (0 < dotProgress && dotProgress < 1) {
+        drawDot(x2, y2, dotProgress, { me: isMe, isStop: isStop });
+      }
     }
-    var alpha = 1 - getLocalProgress( 0.6, 1.0, progress );
-    grad = 'rgba(' + COLOR2.join() + ',' + alpha + ')';
+  });
+})();
+
+function drawLine(x1, y1, x2, y2, progress, params) {
+  if ( !x1 || !y1 || !x2 || !y2 ) {
+    return;
+  }
+
+  var lengthOfLine = getLocalProgress(0, 0.1, progress, params);
+  var m = lengthOfLine;
+  var n = 1 - lengthOfLine;
+  var x3 = n * x1 + m * x2;
+  var y3 = n * y1 + m * y2;
+  var grad = getGrad(x1, y1, x3, y3, lengthOfLine, progress, params);
+
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 1;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x3, y3);
+  ctx.closePath();
+  ctx.strokeStyle = grad;
+  ctx.stroke();
+  ctx.restore();
+}
+
+function getGrad(x1, y1, x2, y2, lengthOfLine, progress, params) {
+  var COLOR1 = params.me ? [ 82, 204, 186 ] : [ 255, 153, 167 ];
+  var COLOR2 = params.me ? [ 255, 248, 59 ] : [ 115, 153, 230 ];
+  var color = [];
+  var grad  = ctx.createLinearGradient(x1, y1, x2, y2);
+
+  if (0 < progress && progress < 0.1) {
+    grad.addColorStop(0, 'rgb(' + COLOR2.join() + ')');
+    grad.addColorStop(1, 'rgb(' + COLOR1.join() + ')');
     return grad;
   }
 
-  function getLocalProgress ( min, max, progress ) {
-    if ( progress < min ) {
-      return 0;
+  if (progress < 0.6) {
+    var localProgress = getLocalProgress(0.1, 0.6, progress);
+    for (var i = 0; i < 3; i ++) {
+      color[i] = (COLOR1[i] + (COLOR2[i] - COLOR1[i]) * localProgress) | 0;
     }
-    if ( progress > max ) {
-      return 1;
-    }
-    return ( progress - min ) * 1 / ( max - min );
+    grad.addColorStop(0, 'rgb(' + COLOR2.join() + ')');
+    grad.addColorStop(1, 'rgb(' + color.join() + ')');
+    return grad;
   }
 
-  //---- sockets
+  var alpha = 1 - getLocalProgress(0.6, 1.0, progress);
+  grad = 'rgba(' + COLOR2.join() + ',' + alpha + ')';
 
-  var socket = io.connect( 'http://pxgrid.net:8000' );
+  return grad;
+}
 
-  socket.on( 'connect', function() {
-    socket.emit( 'client_firstConnect', me );
-  } );
+function drawDot(x, y, progress, params) {
+  var COLOR1 = params.me ? [ 82, 204, 186 ] : [ 255, 153, 167 ];
+  var COLOR2 = params.me ? [ 255, 248, 59 ] : [ 115, 153, 230 ];
+  var color = [];
 
-  socket.on( 'server_myId', function ( params ) {
-    me.id = params.myID;
-    players = params.players;
-  } );
+  var localProgress1 = getLocalProgress(0, 0.1, progress);
+  var r = DOT_RADIUS - 3 + (3 * (params.isStop ? 1 : localProgress1));
+  var r2 = DOT_RADIUS + 10 - (6 * localProgress1);
 
-  socket.on( 'server_sync', function ( allPlayers ) {
-    players = allPlayers;
-    if ( !!me.id ) {
-      socket.emit( 'client_pushMyData', me );
+  for (var i = 0; i < 3; i ++) {
+    var localProgress2 = getLocalProgress(0.5, 0.8, progress);
+    color[i] = (COLOR1[i] + (COLOR2[i] - COLOR1[i]) * localProgress2) | 0;
+  }
+
+  var alpha = 1 - getLocalProgress(0.9, 1, progress);
+
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, false);
+  ctx.fillStyle = 'rgba( ' + color[0] + ', ' + color[1] + ', ' + color[2] + ',' + alpha + ' )';
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(x, y, r2, 0, Math.PI * 2, false);
+  ctx.fillStyle = 'rgba( ' + color[0] + ', ' + color[1] + ', ' + color[2] + ', ' + Math.min(0.2, alpha) + ')';
+  ctx.fill();
+
+  ctx.restore();
+}
+
+function getLocalProgress(min, max, progress) {
+  if (progress < min) {
+    return 0;
+  }
+  if (progress > max) {
+    return 1;
+  }
+  return (progress - min) * 1 / (max - min);
+}
+
+
+//     Underscore.js 1.5.2
+//     http://underscorejs.org
+//     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     Underscore may be freely distributed under the MIT license.
+function throttle(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  options = options || {};
+  var later = function() {
+    previous = options.leading === false ? 0 : new Date();
+    timeout = null;
+    result = func.apply(context, args);
+  };
+  return function() {
+    var now = new Date();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0) {
+      clearTimeout(timeout);
+      timeout = null;
+      previous = now;
+      result = func.apply(context, args);
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
     }
-  } );
+    return result;
+  };
+}
 
-} )();
+})();
