c5a50bed729330ecfc5d418eddeab7a4baaf5e0b
Kazuhito Hokamura <k.hokamura@gmail.com>
2013-09-27 17:51:00 +0900

    Update 2013 with 9e02946

---
+++ 2013/assets/js/src/util.js
@@ -0,0 +1,61 @@
+( function ( $ ) {
+  $( function () {
+
+    // define the root of the main content
+
+    new $.LazyJaxDavis( function ( router ) {
+
+      var $root = $( '.mod-pageMain' );
+      var $navItems = $( '.mod-nav li' );
+
+      router.bind( 'everyfetchstart', function ( page ) {
+        // $root.css( 'opacity', 0.6 );
+        $root.addClass( 'mod-pageMain--changing' );
+        $( 'html, body' ).animate( { scrollTop: 0 } );
+      } );
+
+      router.bind( 'everyfetchsuccess', function ( page ) {
+        // $root.css( 'opacity', 1 );
+        $newcontent = $( page.rip( 'content' ) ).hide();
+        setTimeout( function () {
+          $root.removeClass( 'mod-pageMain--changing' );
+          $root.empty().append( $newcontent );
+          $newcontent.fadeIn();
+          page.trigger( 'pageready' );
+
+          $navItems.each( function () {
+            var $navItem = $( this );
+            var href = $navItem.find( 'a' ).attr( 'href' );
+            var path = location.pathname;
+            if (path.match(/\/$/)){
+              path += 'index.html';
+            }
+            if ( path === href ){
+              $navItem.addClass( 'mod-nav--current' );
+            } else {
+              $navItem.removeClass( 'mod-nav--current' );
+            }
+          } );
+
+        }, 200 );
+      });
+
+      router.bind( 'everyfetchfail', function () {
+        // alert('ajax error!');
+        // $root.css( 'opacity', 1 );
+        $root.removeClass( 'mod-pageMain--changing' );
+      } );
+
+    } );
+
+  } );
+} )( jQuery );
+
+
+( function ( $ ) {
+  $( function () {
+    $( '.mod-pagetop' ).on( 'click', function () {
+      $( 'html, body' ).animate( { scrollTop: 0 } );
+    } );
+  } );
+} )( jQuery );
