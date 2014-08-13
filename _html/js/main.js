3d4dc9fa64411cdba61d8c18f27a69144d6e9b6a
tacamy <takami.yamada@gmail.com>
2014-08-13 15:38:39 +0900

    花火とイントロの高さをJSで算出して設定

--- _html/js/main.js
+++ _html/js/main.js
@@ -1,1 +1,45 @@
+(function ($, window, document, undefined) {
 
+  'use strict';
+
+  var app = {
+
+    run: function () {
+      this.$window = $(window);
+      this.$header = $('.l-header');
+      this.$intro  = $('.l-intro');
+      this.$fireworks  = $('.l-fireworks');
+
+      this.windowHeight = window.innerHeight;
+      this.headerHeight = this.$header.height();
+
+      this.resizeContentsHeight();
+
+      this.eventify();
+    },
+
+    eventify: function () {
+      var self = this;
+
+      self.$window.on('resize', $.proxy(self.resizeContentsHeight, self));
+    },
+
+    resizeContentsHeight: function () {
+      var height;
+
+      this.windowHeight = window.innerHeight;
+      height = this.windowHeight - this.headerHeight;
+
+      this.$intro.css('min-height', height);
+      this.$fireworks.css('min-height', height);
+    }
+
+  };
+
+  window.app = app;
+
+  $(function () {
+    app.run();
+  });
+
+})(jQuery, this, this.document);
