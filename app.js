c382887a4eb61bec125b0af3ea2621ddd171574c
tacamy <takami.yamada@gmail.com>
2015-08-27 18:30:10 +0900

    更地化

--- app.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-
-var port = process.env.PORT || 3000;
-connect(
-  connect.basicAuth('node', 'fest2011'),
-  connect.static(__dirname)
-).listen(port);
-
-console.log('Server running port %d', port);
