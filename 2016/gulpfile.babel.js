1c4ae6688d76313190bc84d6c795cd2f44e3a2ce
leader22 <y.sugiura.0316@gmail.com>
2016-09-17 23:00:15 +0900

    ejsで立て直し

--- 2016/gulpfile.babel.js
+++ 2016/gulpfile.babel.js
@@ -1,43 +1,59 @@
+'use strict';
 import gulp from 'gulp';
-import sass from 'gulp-sass';
 import util from 'gulp-util';
 import plumber from 'gulp-plumber';
+import ejs from 'gulp-ejs';
+import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import cleancss from 'gulp-clean-css';
 
 const browserSync = require('browser-sync').create();
-const base_path = './',
-      paths = {
-        cssDir: base_path + 'css/',
-        html: base_path + '**/*.html'
+const srcPaths = {
+        html: './src/html/*.ejs',
+        css:  './src/scss/main.scss'
       };
+const destPath = './';
 
-gulp.task('css', () => {
-  return gulp.src(paths.cssDir + '**/*.scss')
-      .pipe(sass({ style: 'expanded' }))
-      .pipe(plumber((error) => {
-        util.log(util.colors.red(error.message));
-        gulp.task('css').emit('end');
-      }))
-      .pipe(autoprefixer())
-      .pipe(cleancss())
-      .pipe(gulp.dest(paths.cssDir))
-      .pipe(browserSync.stream());
+gulp.task('html', () => {
+  return gulp
+    .src(srcPaths.html)
+    .pipe(ejs({
+      msg: 'Hello Gulp!'
+    }, { ext: '.html' }))
+    .pipe(plumber((error) => {
+      util.log(util.colors.red(error.message));
+      gulp.task('html').emit('end');
+    }))
+    .pipe(gulp.dest(destPath));
 });
 
-gulp.task('server', ['css'], () => {
+gulp.task('css', () => {
+  return gulp
+    .src(srcPaths.css)
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(plumber((error) => {
+      util.log(util.colors.red(error.message));
+      gulp.task('css').emit('end');
+    }))
+    .pipe(autoprefixer())
+    .pipe(cleancss())
+    .pipe(gulp.dest(destPath))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('server', ['html', 'css'], () => {
   browserSync.init({
     port: 4000,
     server: {
-      baseDir: base_path,
+      baseDir: destPath,
     },
     ui: {
       port: 4001
     }
   });
 
-  gulp.watch(paths.cssDir + '**/*.scss', ['css']);
-  gulp.watch(paths.html).on('change', browserSync.reload);
+  gulp.watch(srcPaths.html, ['html']).on('change', browserSync.reload);
+  gulp.watch(srcPaths.css, ['css']).on('change', browserSync.reload);
 });
 
-gulp.task('default', [ 'css', 'server']);
+gulp.task('default', ['html', 'css', 'server']);
