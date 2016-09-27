eae19189da984f4babd5d4d138f78b3eafe903bc
leader22 <y.sugiura.0316@gmail.com>
2016-09-27 22:52:58 +0900

    indexのjsを整理

--- 2016/src/js/main.js
+++ 2016/src/js/main.js
@@ -1,2 +1,10 @@
-const pathname = location.pathname;
-console.log(pathname);
+const main = location.pathname.split('/2016')[1];
+
+const scripts = {
+  '/':          require('./index'),
+  'index.html': require('./index'),
+  // 'coc.html':   require('./coc')
+};
+
+console.info(main);
+(scripts[main] || function() {})();
