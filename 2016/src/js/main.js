1df27098b699fd2bcfe1af9f2e9acaa05e4caf30
leader22 <y.sugiura.0316@gmail.com>
2016-10-05 15:27:18 +0900

    add google-map to venue

--- 2016/src/js/main.js
+++ 2016/src/js/main.js
@@ -1,10 +1,11 @@
 const main = location.pathname.split('/2016')[1];
 
 const scripts = {
-  '/':          require('./index'),
-  'index.html': require('./index'),
+  '/':           require('./index'),
+  '/index.html':  require('./index'),
+  '/venues.html': require('./index'),
   // 'coc.html':   require('./coc')
 };
 
-console.info(main);
+console.info(main, scripts[main]);
 (scripts[main] || function() {})();
