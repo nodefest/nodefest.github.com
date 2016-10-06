6aa64adc26eea84db3c256eda28e0e7d9152702c
leader22 <y.sugiura.0316@gmail.com>
2016-10-06 10:43:16 +0900

    googleMapのjsをリファクタ & 動かせないように

--- 2016/src/js/_google-map.js
+++ 2016/src/js/_google-map.js
@@ -1,4 +1,6 @@
 module.exports = function() {
+
+  var mapElement = document.getElementById('venue');
   var mapOptions = {
     center: new google.maps.LatLng(35.659676,139.698486),
     zoom: 16,
@@ -12,7 +14,7 @@ module.exports = function() {
     scrollwheel: false,
     panControl: true,
     streetViewControl: true,
-    draggable : true,
+    draggable : false,
     overviewMapControl: true,
     overviewMapControlOptions: {
       opened: false,
@@ -20,29 +22,32 @@ module.exports = function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}],
   };
-  var mapElement = document.getElementById('venue');
+
   var map = new google.maps.Map(mapElement, mapOptions);
-  var locations = [
-    ['渋谷マークシティ', '東京都渋谷区道玄坂1-12-1 渋谷マークシティ13F', 'undefined', 'undefined', 'undefined', 35.658363,  139.698829, 'img/pin-conference.png'],['イベント&コミュニティスペース dots.', '東京都渋谷区宇田川町20-17 NOF渋谷公園通りビル 8F', 'undefined', 'undefined', 'undefined', 35.661232,  139.700330, 'img/pin-workshop.png']
-  ];
-  for (i = 0; i < locations.length; i++) {
-    if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-    if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-    if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-    if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-    if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = {
-      url: locations[i][7],
-      scaledSize: new google.maps.Size(32, 32)
-    };}
-    marker = new google.maps.Marker({
-      icon: markericon,
-      position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+
+  [
+    {
+      title: '渋谷マークシティ',
+      desc:  '東京都渋谷区道玄坂1-12-1 渋谷マークシティ13F',
+      lat:   35.658363,
+      lng:   139.698829,
+      icon:  'img/pin-conference.png'
+    },
+    {
+      title: 'イベント&コミュニティスペース dots.',
+      desc:  '東京都渋谷区宇田川町20-17 NOF渋谷公園通りビル 8F',
+      lat:   35.661232,
+      lng:   139.700330,
+      icon:  'img/pin-workshop.png'
+    }
+  ].forEach(function(locate) {
+    new google.maps.Marker({
       map: map,
-      title: locations[i][0],
-      desc: description,
-      tel: telephone,
-      email: email,
-      web: web
+      title: locate.title,
+      desc: locate.desc,
+      position: new google.maps.LatLng(locate.lat, locate.lng),
+      icon: { url: locate.icon, scaledSize: new google.maps.Size(32, 32) },
     });
-  }
+  });
+
 };
