a5ba338eeff22440acc6077db7c6376df4cfca77
leader22 <y.sugiura.0316@gmail.com>
2016-10-06 11:28:13 +0900

    Escでも閉じるしレイヤーをさわっても閉じる

--- 2016/src/js/module/speaker-overlay.js
+++ 2016/src/js/module/speaker-overlay.js
@@ -2,7 +2,7 @@ module.exports = function() {
 
   var body = document.body;
   var opener = [].slice.call(document.querySelectorAll('.speaker'));
-  var closer = [].slice.call(document.querySelectorAll('.overlay-button'));
+  var closer = [].slice.call(document.querySelectorAll('.overlay'));
 
   opener.forEach(function(el) {
     el.addEventListener('click', _openSpeaker, false);
@@ -10,6 +10,7 @@ module.exports = function() {
   closer.forEach(function(el) {
     el.addEventListener('click', _closeSpeaker, false);
   });
+  document.addEventListener('keydown', _closeSpeakerByEsc, false);
 
   function _openSpeaker(ev) {
     ev.preventDefault();
@@ -21,7 +22,23 @@ module.exports = function() {
 
   function _closeSpeaker(ev) {
     ev.preventDefault();
+    var overlay = ev.currentTarget;
+    var session = overlay.querySelector('.session');
+
+    // DOCUMENT_POSITION_FOLLOWING OR DOCUMENT_POSITION_CONTAINED_BY
+    if (session.compareDocumentPosition(ev.target) !== 20) {
+      body.classList.remove('is-expand');
+      overlay.classList.remove('is-expand');
+    }
+  }
+
+  function _closeSpeakerByEsc(ev) {
+    // Esc only
+    if (ev.keyCode !== 27) { return; }
+
     var overlay = document.querySelector('.overlay.is-expand');
+    if (overlay === null) { return; }
+
     body.classList.remove('is-expand');
     overlay.classList.remove('is-expand');
   }
