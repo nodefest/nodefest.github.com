d24e5a604f93a685fef198b44bdf61ad83071452
leader22 <y.sugiura.0316@gmail.com>
2016-10-10 11:20:09 +0900

    mapのjs整理

--- 2016/src/js/module/google-map.js
+++ 2016/src/js/module/google-map.js
@@ -1,4 +1,4 @@
-module.exports = function() {
+var initialize = function() {
 
   var mapElement = document.getElementById('venue');
   var mapOptions = {
@@ -20,7 +20,82 @@ module.exports = function() {
       opened: false,
     },
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}],
+    styles: [
+      {
+        featureType: 'landscape.man_made',
+        elementType: 'geometry',
+        stylers: [ { color: '#f7f1df' } ]
+      },
+      {
+        featureType: 'landscape.natural',
+        elementType: 'geometry',
+        stylers: [ { color : '#d0e3b4' } ]
+      },
+      {
+        featureType: 'landscape.natural.terrain',
+        elementType: 'geometry',
+        stylers: [ { visibility: 'off' } ]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers:[ { visibility: 'off' } ]
+      },
+      {
+        featureType: 'poi.business',
+        elementType: 'all',
+        stylers:[ { visibility: 'off' } ]
+      },
+      { featureType: 'poi.medical',
+        elementType: 'geometry',
+        stylers: [ { color:'#fbd3da' } ]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [ { color:'#bde6ab' } ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [ { visibility:'off' } ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels',
+        stylers: [ { visibility:'off' } ]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [ { color:'#ffe15f' } ]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [ {color: '#efd151' } ]
+      },
+      {
+        featureType:'road.arterial',
+        elementType:'geometry.fill',
+        stylers: [ { color:'#ffffff' } ]
+      },
+      {
+        featureType:'road.local',
+        elementType:'geometry.fill',
+        stylers:[{color:'black'}]
+      },
+      {
+        featureType:'transit.station.airport',
+        elementType:'geometry.fill',
+        stylers:[{color:'#cfb2db'}]
+      },
+      {
+        featureType:'water',
+        elementType:'geometry',
+        stylers:[{color:'#a2daf2'}]
+      }
+    ]
   };
 
   var map = new google.maps.Map(mapElement, mapOptions);
@@ -49,5 +124,8 @@ module.exports = function() {
       icon: { url: locate.icon, scaledSize: new google.maps.Size(32, 32) },
     });
   });
+};
 
+module.exports = function() {
+  google.maps.event.addDomListener(window, 'load', initialize);
 };
