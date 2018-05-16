0c7137cb018def583b282afa88a8fd617ce2b8eb
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-05-16 11:02:17 +0900

    Remove venue info

--- 2018/src/js/page/index.js
+++ 2018/src/js/page/index.js
@@ -1,4 +1,4 @@
 module.exports = function() {
-  require('../module/google-map')();
-  require('../module/speaker-overlay')();
+  // require('../module/google-map')();
+  // require('../module/speaker-overlay')();
 };
