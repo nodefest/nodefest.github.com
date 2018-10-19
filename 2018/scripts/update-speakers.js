39bd01d7beb49388dda8248c57412bb923db9c78
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-10-19 17:00:39 +0900

    Refactor scripts

--- 2018/scripts/update-speakers.js
+++ 2018/scripts/update-speakers.js
@@ -1,16 +1,88 @@
-#!/usr/bin/env node
-const csvParse = require('csv-parse');
-const https = require('https');
-const Case = require('case');
 const path = require('path');
 const fs = require('fs');
+
+const Case = require('case');
+const csvParse = require('csv-parse');
 const fetch = require('node-fetch');
 const sharp = require('sharp');
-const LINK = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSqG4M_25FXRd5oiGOvueDE4HUnJzI8k-eeTlSHAyzUYvlb_8PN_nLIz3tklnfnDNydsxP7KMPsEOhj/pub?gid=2105877456&single=true&output=csv';
+
+const DOCS_LINK = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSqG4M_25FXRd5oiGOvueDE4HUnJzI8k-eeTlSHAyzUYvlb_8PN_nLIz3tklnfnDNydsxP7KMPsEOhj/pub?gid=2105877456&single=true&output=csv';
+const JSON_PATH = path.normalize(`${__dirname}/../src/json/speakers.json`);
+
+(async function main() {
+  const speakers = await fetchSpeakers(DOCS_LINK);
+
+  await downloadAndReplaceImages(speakers);
+  Object.values(speakers).forEach(speaker => delete speaker.photo);
+
+  fs.writeFileSync(
+    JSON_PATH,
+    JSON.stringify(speakers, null, 2)
+  );
+  console.log('Finished Updating');
+}()).catch(err => {
+  console.log(err.stack);
+  process.exit(1);
+});
+
+async function fetchSpeakers (url) {
+  console.log(`Downloading csv from: ${url}`);
+  const res = await fetch(url);
+
+  return new Promise((resolve, reject) => {
+    const allSpeakers = {};
+
+    const parser = res.body.pipe(csvParse({
+      auto_parse: true,
+      columns: true
+    }));
+
+    parser.on('error', reject);
+
+    parser.on('data', data => {
+      data = removeEmptyFields(data);
+      data = removeKeySecondLine(data);
+
+      data.name = combineNames(data.firstName, data.surname, ' ');
+      data['氏名'] = combineNames(data['姓'], data['名'], '　');
+      data.name = data.name || data.nickName || data['氏名'];
+      data['氏名'] = data['氏名'] || data.nickName || data.name;
+
+      if (!data.twitter) {
+        console.log(`Received entry without twitter account (O_O): ${data.name}`);
+        return;
+      }
+
+      allSpeakers[data.twitter] = data;
+      console.log(`Received: ${data.twitter}`);
+    });
+
+    parser.on('end', () => {
+      console.log('Finished csv.');
+      resolve(allSpeakers);
+    });
+  });
+}
+
+async function downloadAndReplaceImage (speaker) {
+  console.log(`Started downloading: ${speaker.twitter} image`);
+  const res = await fetch(speaker.photo);
+  const imgBuffer = await res.buffer();
+  await sharp(imgBuffer)
+      .resize(512, 512, { fit: 'outside', withoutEnlargement: true })
+      .toFile(`${__dirname}/../img/speakers/pic-${speaker.twitter}.jpg`);
+
+  console.log(`Downloaded: ${speaker.twitter} - file`);
+}
+
+async function downloadAndReplaceImages (speakers) {
+  await Promise.all(Object.values(speakers).map(downloadAndReplaceImage));
+  console.log('All Images done');
+}
 
 function removeEmptyFields (data) {
-  for (let key in data) {
-    let value = data[key];
+  for (const key in data) {
+    const value = data[key];
     if (/^\s*-\s*$/.test(value)) {
       delete data[key];
     }
@@ -19,8 +91,8 @@ function removeEmptyFields (data) {
 }
 
 function removeKeySecondLine (data) {
-  for (let key in data) {
-    let keyStraight = Case.camel(/^.*$/m.exec(key)[0]);
+  for (const key in data) {
+    const keyStraight = Case.camel(/^.*$/m.exec(key)[0]);
     if (key !== keyStraight) {
       data[keyStraight] = data[key];
       delete data[key];
@@ -36,73 +108,3 @@ function combineNames (a, b, space) {
   return b || undefined;
 }
 
-function getSpeakers () {
-  console.log(`Downloading csv from: ${LINK}`);
-  return fetch(LINK)
-    .then(res => new Promise((resolve, reject) => {
-      const parser = res.body.pipe(csvParse({
-        auto_parse: true,
-        columns: true
-      }));
-      const allSpeakers = {};
-      parser.on('error', reject);
-      parser.on('data', data => {
-        data = removeEmptyFields(data);
-        data = removeKeySecondLine(data);
-        data.name = combineNames(data.firstName, data.surname, ' ');
-        data['氏名'] = combineNames(data['姓'], data['名'], '　');
-        data.name = data.name || data.nickName || data['氏名'];
-        data['氏名'] = data['氏名'] || data.nickName || data.name;
-        if (!data.twitter) {
-          console.log(`Received entry without twitter account (O_O): ${data.name}`)
-          return
-        }
-        allSpeakers[data.twitter] = data;
-        console.log(`Received: ${data.twitter}`);
-      });
-      parser.on('end', () => {
-        console.log('Finished csv.');
-        resolve(allSpeakers);
-      });
-    }));
-}
-
-function downloadAndReplaceImage (speaker) {
-  console.log(`Started downloading: ${speaker.twitter} image`)
-  return fetch(speaker.photo)
-    .then(res => res.buffer())
-    .then(imgBuffer => sharp(imgBuffer)
-      .resize(512, 512, { fit: 'outside', withoutEnlargement: true })
-      .toFile(`${__dirname}/../img/speakers/pic-${speaker.twitter}.jpg`))
-    .then(() => {
-      console.log(`Downloaded: ${speaker.twitter} - file`);
-      delete speaker.photo;
-      return speaker;
-    })
-    .catch(error => {
-      console.warn(`Error downloading ${speaker.twitter} from ${speaker.photo}: ${error.stack}`);
-      return speaker;
-    })
-}
-
-function downloadAndReplaceImages (speakers) {
-  return Promise.all(Object.values(speakers).map(speaker => downloadAndReplaceImage(speaker)))
-    .then(() => {
-      console.log('All Images done')
-      return speakers
-    });
-}
-
-getSpeakers()
-  .then(speakers => downloadAndReplaceImages(speakers))
-  .then(speakers => {
-    fs.writeFileSync(
-      path.normalize(`${__dirname}/../src/json/speakers.json`),
-      JSON.stringify(speakers, null, 2)
-    );
-    console.log('Finished Updating');
-  })
-  .catch(errors => {
-    console.log(errors.stack);
-    process.exit(1);
-  })
