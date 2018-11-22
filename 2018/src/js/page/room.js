403d7773419655977611bf8cb4c2f2804f2d671b
Martin Heidegger <martin.heidegger@gmail.com>
2018-11-22 12:44:13 +0900

    If a room is selected, let it contain all the list entries.

--- 2018/src/js/page/room.js
+++ 2018/src/js/page/room.js
@@ -4,23 +4,26 @@ module.exports = function () {
     var overview = document.getElementById('rooms')
     var rooms = [].slice.call(document.querySelectorAll('.room-list'))
     var allParts = rooms.concat(overview)
+    rooms = rooms.map(function (roomNode) {
+        var entries = [].slice.call(roomNode.querySelectorAll('li'))
+        return {
+            node: roomNode,
+            entries: entries
+        }
+    })
     hashChange.addListener(function (hash) {
         var parts = /^[\/|#]?([^-]+)-(.+)/.exec(hash);
         var day = parts && parts[1];
         var roomName = parts && parts[2];
-        var selectedPart
+        var selectedRoom
         if (day) {
             var lookupId = 'list-' + day + '-' + roomName;
-            console.log(parts)
-            selectedPart = rooms.filter(function (part) {
-                console.log(lookupId, '...', part.id)
-                return part.id === lookupId;
+            selectedRoom = rooms.filter(function (part) {
+                return part.node.id === lookupId;
             })[0];
         }
-        if (!selectedPart) {
-            selectedPart = overview
-        }
         console.log(allParts)
+        var selectedPart = (selectedRoom && selectedRoom.node) || overview
         allParts.forEach(function (part) {
             if (part === selectedPart) {
                 part.style.display = ''
