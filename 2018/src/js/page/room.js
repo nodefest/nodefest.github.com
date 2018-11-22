515149dc0f58053491f819936e76f792aaa8ab15
Martin Heidegger <martin.heidegger@gmail.com>
2018-11-22 13:56:51 +0900

    Showing only the list items that are relevant right now.

--- 2018/src/js/page/room.js
+++ 2018/src/js/page/room.js
@@ -1,6 +1,12 @@
 var hashChange = require('../vendor/hashChange');
 var momentTz = require('moment-timezone');
 
+function replaceFlag (oldNode, newNode, flag) {
+    oldNode && oldNode.classList.remove(flag)
+    newNode && newNode.classList.add(flag)
+    return newNode
+}
+
 module.exports = function () {
     var overview = document.getElementById('rooms')
     var timeNode = document.getElementById('room-time')
@@ -8,6 +14,9 @@ module.exports = function () {
     var allParts = rooms.concat(overview)
     rooms = rooms.map(function (roomNode) {
         var entries = [].slice.call(roomNode.querySelectorAll('li'))
+        entries.forEach(function (entryNode) {
+            entryNode.start = momentTz(entryNode.attributes['data-start'].value).tz('Asia/Tokyo')
+        })
         return {
             node: roomNode,
             entries: entries
@@ -29,7 +38,6 @@ module.exports = function () {
                 return part.node.id === lookupId;
             })[0];
         }
-        console.log(allParts)
         var selectedPart = (selectedRoom && selectedRoom.node) || overview
         allParts.forEach(function (part) {
             if (part === selectedPart) {
@@ -39,6 +47,11 @@ module.exports = function () {
             }
         })
     })
+
+    var before
+    var after
+    var current
+
     function render () {
         var now = momentTz(Date.now()).tz('Asia/Tokyo')
         if (hour) {
@@ -48,6 +61,25 @@ module.exports = function () {
             now.minutes(minute)
         }
         timeNode.innerText = now.format('HH:mm')
+        if (!selectedRoom) {
+            return
+        }
+        var newBefore
+        var newCurrent
+        var newAfter
+        selectedRoom.entries.forEach(function (entryNode) {
+            var isBefore = entryNode.start.hours() < now.hours() ||
+                entryNode.start.hours() === now.hours() && entryNode.start.minutes() < now.minutes()
+            if (isBefore) {
+                newBefore = newCurrent
+                newCurrent = entryNode
+            } else if(!newAfter) {
+                newAfter = entryNode
+            }
+        })
+        current = replaceFlag(current, newCurrent, 'room-entry-current')
+        before = replaceFlag(before, newBefore, 'room-entry-before')
+        after = replaceFlag(after, newAfter, 'room-entry-after')
     }
     render()
     setInterval(render, 100)
