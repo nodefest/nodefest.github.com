18f69dec069accbc3a7705fe3fe3e022449b9cab
Martin Heidegger <martin.heidegger@gmail.com>
2018-11-22 12:53:10 +0900

    Added hour/minute override to hash (to only select a certain time)

--- 2018/src/js/page/room.js
+++ 2018/src/js/page/room.js
@@ -14,10 +14,14 @@ module.exports = function () {
         }
     })
     var selectedRoom
+    var hour
+    var minute
     hashChange.addListener(function (hash) {
-        var parts = /^[\/|#]?([^-]+)-(.+)/.exec(hash);
+        var parts = /^[\/|#]?([^-]+)-([^-]+)(-(\d{2}):(\d{2}))?/.exec(hash);
         var day = parts && parts[1];
         var roomName = parts && parts[2];
+        hour = parts && parts[4];
+        minute = parts && parts[5];
         selectedRoom = null
         if (day) {
             var lookupId = 'list-' + day + '-' + roomName;
@@ -37,6 +41,12 @@ module.exports = function () {
     })
     function render () {
         var now = momentTz(Date.now()).tz('Asia/Tokyo')
+        if (hour) {
+            now.hours(hour)
+        }
+        if (minute) {
+            now.minutes(minute)
+        }
         timeNode.innerText = now.format('HH:mm')
     }
     render()
